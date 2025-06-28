9ea300f4513f370e3823c82e6e83dcb7d18afc54
naveen-2111-dev <naveenrajanm9@gmail.com>
2025-06-28 11:01:50 -0700

    any thing

--- utils/generateSecret.ts
+++ utils/generateSecret.ts
@@ -1,6 +1,7 @@
 import crypto from 'crypto';
 
 export async function GenerateSecret(pool: string) {
+    //generates webhook secret
     return crypto.createHmac("sha256", process.env.MASTER_SECRET!)
         .update(pool)
         .digest("hex");
